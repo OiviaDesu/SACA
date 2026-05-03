7792a1c0f8bedc9d8c729a260eb8aca4fff4e07f
JHNXY <s3652190@student.rmit.edu.au>
2026-05-03 23:14:43 +1000

    Update UI Body diagram

--- lib/presentation/widgets/body_diagram.dart
+++ lib/presentation/widgets/body_diagram.dart
@@ -56,8 +56,16 @@ class BodyDiagram extends StatelessWidget {
                 return Stack(
                   children: [
                     Positioned.fill(
-                      child: CustomPaint(
-                        painter: _BodyPanelPainter(view: view),
+                      child: IgnorePointer(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          child: Image.asset(
+                            view == BodyView.front
+                                ? 'assets/images/Body_front.png'
+                                : 'assets/images/Body_back.png',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
                       ),
                     ),
                     for (final area in areas)
@@ -100,6 +108,7 @@ class _PositionedAreaChip extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final point = _positionFor(area.id);
+
     return Positioned(
       left: point.dx * size.width,
       top: point.dy * size.height,
@@ -131,6 +140,7 @@ class _PositionedAreaChip extends StatelessWidget {
       'leg' => const Offset(0.03, 0.76),
       'knees' => const Offset(0.68, 0.80),
       'toes' => const Offset(0.64, 0.92),
+
       'ears' => const Offset(0.68, 0.09),
       'neck' => const Offset(0.03, 0.19),
       'shoulder' => const Offset(0.66, 0.19),
@@ -140,173 +150,8 @@ class _PositionedAreaChip extends StatelessWidget {
       'finger' => const Offset(0.68, 0.66),
       'lower_leg' => const Offset(0.03, 0.79),
       'ankle' => const Offset(0.02, 0.93),
+
       _ => const Offset(0.05, 0.05),
     };
-  }
-}
-
-class _BodyPanelPainter extends CustomPainter {
-  const _BodyPanelPainter({required this.view});
-
-  final BodyView view;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final panel = Paint()
-      ..shader = const LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          Color(0xFFFDF8F6),
-          Color(0xFFF2FBFD),
-        ],
-      ).createShader(Offset.zero & size);
-    final guide = Paint()
-      ..color = const Color(0xFFD9E7EB)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 1.2;
-
-    final frame = RRect.fromRectAndRadius(
-      Offset.zero & size,
-      const Radius.circular(22),
-    );
-    canvas.drawRRect(frame, panel);
-    canvas.drawRRect(frame, guide);
-
-    final silhouetteFill = Paint()
-      ..color = const Color(0x22B9DCEA)
-      ..style = PaintingStyle.fill;
-    final silhouetteLine = Paint()
-      ..color = const Color(0xFF8FAAB0)
-      ..strokeWidth = 2.2
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeJoin = StrokeJoin.round;
-
-    final centerX = size.width * 0.52;
-    final top = size.height * 0.06;
-
-    final head = Rect.fromCenter(
-      center: Offset(centerX, top + size.height * 0.10),
-      width: size.width * 0.16,
-      height: size.height * 0.14,
-    );
-    canvas.drawOval(head, silhouetteFill);
-    canvas.drawOval(head, silhouetteLine);
-
-    final shoulderY = top + size.height * 0.18;
-    final hipY = top + size.height * 0.54;
-    final legEndY = top + size.height * 0.90;
-    final bodyWidth = size.width * 0.18;
-    final armReach = size.width * 0.13;
-
-    final torso = Path()
-      ..moveTo(centerX - bodyWidth, shoulderY)
-      ..quadraticBezierTo(
-        centerX - bodyWidth * 1.25,
-        shoulderY + size.height * 0.08,
-        centerX - bodyWidth * 0.92,
-        hipY,
-      )
-      ..quadraticBezierTo(
-        centerX - bodyWidth * 0.15,
-        hipY + size.height * 0.03,
-        centerX,
-        hipY,
-      )
-      ..quadraticBezierTo(
-        centerX + bodyWidth * 0.15,
-        hipY + size.height * 0.03,
-        centerX + bodyWidth * 0.92,
-        hipY,
-      )
-      ..quadraticBezierTo(
-        centerX + bodyWidth * 1.25,
-        shoulderY + size.height * 0.08,
-        centerX + bodyWidth,
-        shoulderY,
-      )
-      ..close();
-    canvas.drawPath(torso, silhouetteFill);
-    canvas.drawPath(torso, silhouetteLine);
-
-    final leftArm = Path()
-      ..moveTo(centerX - bodyWidth, shoulderY + size.height * 0.02)
-      ..quadraticBezierTo(
-        centerX - bodyWidth - armReach,
-        shoulderY + size.height * 0.16,
-        centerX - bodyWidth * 1.05,
-        shoulderY + size.height * 0.38,
-      )
-      ..quadraticBezierTo(
-        centerX - bodyWidth * 0.9,
-        shoulderY + size.height * 0.46,
-        centerX - bodyWidth * 0.85,
-        shoulderY + size.height * 0.54,
-      );
-    final rightArm = Path()
-      ..moveTo(centerX + bodyWidth, shoulderY + size.height * 0.02)
-      ..quadraticBezierTo(
-        centerX + bodyWidth + armReach,
-        shoulderY + size.height * 0.16,
-        centerX + bodyWidth * 1.05,
-        shoulderY + size.height * 0.38,
-      )
-      ..quadraticBezierTo(
-        centerX + bodyWidth * 0.9,
-        shoulderY + size.height * 0.46,
-        centerX + bodyWidth * 0.85,
-        shoulderY + size.height * 0.54,
-      );
-    canvas.drawPath(leftArm, silhouetteLine);
-    canvas.drawPath(rightArm, silhouetteLine);
-
-    final leftLeg = Path()
-      ..moveTo(centerX - size.width * 0.04, hipY)
-      ..quadraticBezierTo(
-        centerX - size.width * 0.09,
-        hipY + size.height * 0.20,
-        centerX - size.width * 0.07,
-        legEndY,
-      );
-    final rightLeg = Path()
-      ..moveTo(centerX + size.width * 0.04, hipY)
-      ..quadraticBezierTo(
-        centerX + size.width * 0.09,
-        hipY + size.height * 0.20,
-        centerX + size.width * 0.07,
-        legEndY,
-      );
-    canvas.drawPath(leftLeg, silhouetteLine);
-    canvas.drawPath(rightLeg, silhouetteLine);
-
-    if (view == BodyView.front) {
-      canvas.drawLine(
-        Offset(centerX - size.width * 0.03, shoulderY + size.height * 0.10),
-        Offset(centerX + size.width * 0.05, shoulderY + size.height * 0.10),
-        silhouetteLine,
-      );
-      canvas.drawCircle(
-        Offset(centerX + size.width * 0.05, shoulderY + size.height * 0.08),
-        5,
-        silhouetteLine,
-      );
-    } else {
-      canvas.drawLine(
-        Offset(centerX, shoulderY + size.height * 0.01),
-        Offset(centerX, hipY - size.height * 0.01),
-        silhouetteLine,
-      );
-      canvas.drawLine(
-        Offset(centerX - size.width * 0.07, shoulderY + size.height * 0.12),
-        Offset(centerX + size.width * 0.07, shoulderY + size.height * 0.12),
-        silhouetteLine,
-      );
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant _BodyPanelPainter oldDelegate) {
-    return oldDelegate.view != view;
   }
 }
